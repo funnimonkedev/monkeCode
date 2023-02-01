248ddbf02dd7afad4f68ed577490cdf49b1be2ca
funnimonkedev <113639481+funnimonkedev@users.noreply.github.com>
2023-02-01 17:06:27 -0500

    Update compiler.js

--- compiler.js
+++ compiler.js
@@ -1,44 +1,30 @@
 function tokenizer(input) {
 
-  // A `current` variable for tracking our position in the code like a cursor.
-  let current = 0;
 
-  // And a `tokens` array for pushing our tokens to.
+  let current = 0;
+  
   let tokens = [];
 
-  // We start by creating a `while` loop where we are setting up our `current`
-  // variable to be incremented as much as we want `inside` the loop.
-  //
-  // We do this because we may want to increment `current` many times within a
-  // single loop because our tokens can be any length.
   while (current < input.length) {
 
-    // We're also going to store the `current` character in the `input`.
+
     let char = input[current];
 
-    // The first thing we want to check for is an open parenthesis. This will
-    // later be used for `CallExpression` but for now we only care about the
-    // character.
-    // We check to see if we have an open parenthesis:
+
     if (char === '(') {
 
-      // If we do, we push a new token with the type `paren` and set the value
-      // to an open parenthesis.
+
       tokens.push({
         type: 'paren',
         value: '(',
       });
 
-      // Then we increment `current`
       current++;
 
-      // And we `continue` onto the next cycle of the loop.
       continue;
     }
 
-    // Next we're going to check for a closing parenthesis. We do the same exact
-    // thing as before: Check for a closing parenthesis, add a new token,
-    // increment `current`, and `continue`.
+
     if (char === ')') {
       tokens.push({
         type: 'paren',
@@ -48,17 +34,13 @@ function tokenizer(input) {
       continue;
     }
 
-    // So here we're just going to test for existence and if it does exist we're
-    // going to just `continue` on.
     let WHITESPACE = /\s/;
     if (WHITESPACE.test(char)) {
       current++;
       continue;
     }
 
-    // The next type of token is a number. This is different than what we have
-    // seen before because a number could be any number of characters and we
-    // want to capture the entire sequence of characters as one token.
+
     if(char === '☻') {
       console.log("you found the hidden secret!");
       tokens.push({
@@ -72,8 +54,7 @@ function tokenizer(input) {
     let NUMBERS = /[0-9]/;
     if (NUMBERS.test(char)) {
 
-      // We're going to create a `value` string that we are going to push
-      // characters to.
+
       let value = '';
 
       while (NUMBERS.test(char)) {
@@ -82,23 +63,19 @@ function tokenizer(input) {
       }
 
 
-      // After that we push our `number` token to the `tokens` array.
       tokens.push({ type: 'number', value });
 
       // And we continue on.
       continue;
     }
 
-    // We'll start by checking for the opening quote:
+
     if (char === '"') {
-      // Keep a `value` variable for building up our string token.
+
       let value = '';
 
-      // We'll skip the opening double quote in our token.
       char = input[++current];
 
-      // Then we'll iterate through each character until we reach another
-      // double quote.
       while (char !== '"') {
         value += char;
         char = input[++current];
@@ -107,7 +84,6 @@ function tokenizer(input) {
       // Skip the closing double quote.
       char = input[++current];
 
-      // And add our `string` token to the `tokens` array.
       tokens.push({ type: 'string', value });
 
       continue;
@@ -116,13 +92,13 @@ function tokenizer(input) {
     let LETTERS = /[a-z]/i;
     if (LETTERS.test(char)) {
       let value = '';
-      // Again we're just going to loop through all the letters pushing them to
-      // a value.
+
+     
       while (LETTERS.test(char)) {
         value += char;
         char = input[++current];
       }
-      // And pushing that value as a token with the type `name` and continuing.
+
       tokens.push({ type: 'name', value });
 
       continue;
